b05fc9a9ad9fe6543dd14c95c52b3ab21c4ff1d2
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-31 22:38:11 +0300

    Unnecessary files removed

--- courseinfo/src/App.tsx
+++ courseinfo/src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+    const courseName = "Half Stack application development";
+    const courseParts = [
+        {
+            name: "Fundamentals",
+            exerciseCount: 10,
+        },
+        {
+            name: "Using props to pass data",
+            exerciseCount: 7,
+        },
+        {
+            name: "Deeper type usage",
+            exerciseCount: 14,
+        },
+    ];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>{courseName}</h1>
+            <p>
+                {courseParts[0].name} {courseParts[0].exerciseCount}
+            </p>
+            <p>
+                {courseParts[1].name} {courseParts[1].exerciseCount}
+            </p>
+            <p>
+                {courseParts[2].name} {courseParts[2].exerciseCount}
+            </p>
+            <p>
+                Number of exercises{" "}
+                {courseParts.reduce(
+                    (carry, part) => carry + part.exerciseCount,
+                    0
+                )}
+            </p>
+        </div>
+    );
+};
 
 export default App;
